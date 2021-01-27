37a3d131aa06d86a751dce61e62af454330a23f9
Anna Vasileva <vasilec1992@yandex.rum>
2021-01-27 16:57:22 +0100

    correction

--- 2-dz/5.js
+++ 2-dz/5.js
@@ -1,35 +1,30 @@
-const sum = (a,b) =>{
-   return a+b
-}
+const sum = (a, b) => {
+  return a + b;
+};
 
-const subtraction = (a,b) =>{
-   return a-b
-}
+const subtraction = (a, b) => {
+  return a - b;
+};
 
-const multiplication = (a,b) =>{
-   return a*b
-}
+const multiplication = (a, b) => {
+  return a * b;
+};
 
-const division = (a,b) =>{
-   return a/b
-}
+const division = (a, b) => {
+  return a / b;
+};
 
-const mathOperation = (arg1, arg2, operation)=>{
-   switch(operation){
-      case "+":
-         return sum(arg1, arg2)
-      break
-      case "-":
-         return subtraction(arg1, arg2)
-      break
-      case "*":
-         return multiplication(arg1, arg2)
-      break
-      case "/":
-         return division(arg1, arg2)
-      break
-   }
-}
+const mathOperation = (arg1, arg2, operation) => {
+  switch (operation) {
+    case "+":
+      return sum(arg1, arg2);
+    case "-":
+      return subtraction(arg1, arg2);
+    case "*":
+      return multiplication(arg1, arg2);
+    case "/":
+      return division(arg1, arg2);
+  }
+};
 
-console.log(mathOperation(5,5,"+"))
-
+console.log(mathOperation(5, 5, "+"));
