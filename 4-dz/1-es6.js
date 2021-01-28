13c230ba1d60320cd293f3dc9f6132d72b1c47dd
Anna Vasileva <vasilec1992@yandex.rum>
2021-01-28 14:14:23 +0100

    4 homework

---
+++ 4-dz/1-es6.js
@@ -0,0 +1,21 @@
+/* 1.1 Сделайте в стиле es5, а затем в стиле es6, конструктор Product, который принимает параметры name и price, сохраните
+их как свойства объекта. Также объекты типа Product должны иметь метод make25PercentDiscount, который будет
+уменьшать цену в объекте на 25%. Имейте в виду, что метод make25PercentDiscount не должен быть внутри
+функции-конструктора, и также не нужно создавать отдельный объект-прототип (как объект transport в уроке). */
+
+"use strict";
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  make25PercentDiscount() {
+    this.price = this.price - this.price * 0.25;
+  }
+}
+
+let obj1 = new Product("book", 45);
+obj1.make25PercentDiscount();
+console.log(obj1);
